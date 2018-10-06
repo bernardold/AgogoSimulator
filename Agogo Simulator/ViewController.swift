fbdd850682784ecc923e2204ec4c6899260d30d8
Bernardo Duarte <bernardoduarte@tokenlab.com.br>
2018-10-06 19:17:29 -0300

    Initial Commit

---
+++ Agogo Simulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Agogo Simulator
+//
+//  Created by Bernardo Duarte on 06/10/18.
+//  Copyright © 2018 Bernardo Duarte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
